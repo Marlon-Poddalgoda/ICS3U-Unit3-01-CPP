dbc9f7f96cb2e0f2868723ac6835b99f5728aa5f
Ubuntu <ubuntu@ip-172-31-11-30.ec2.internal>
2020-11-27 18:03:49 +0000

    added basic_addition.cpp

---
+++ basic_addition.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2020 Marlon Poddalgoda, All rights reserved
+//
+// Created by Marlon Poddalgoda
+// Created on November 2020
+// This program adds 2 user inputted numbers
+
+#include <iostream>
+
+int main() {
+    // this function calculates the sum of 2 user inputs
+    int firstNumber;
+    int secondNumber;
+    int answer;
+
+    // input
+    std::cout << "Enter your first number: ";
+    std::cin >> firstNumber;
+    std::cout << "Enter your second number: ";
+    std::cin >> secondNumber;
+
+    // process
+    answer = firstNumber + secondNumber;
+
+    // output
+    std::cout << "" << std::endl;
+    std::cout << firstNumber << " + " << secondNumber << " = "
+              << answer << std::endl;
+}
